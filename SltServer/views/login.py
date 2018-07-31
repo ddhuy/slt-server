0d7a6de434c188b7d204af40a13580a2eac4cb88
Huy Dang <ddh.huy@gmail.com>
2018-07-31 15:51:07 +0700

    password change

--- SltServer/views/login.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.views import LoginView
-
-class LoginPage ( LoginView ) :
-    template_name = "login.html"
